db33161b329acce73c2f06348c4aef423f222b94
hulklee1 <35159494+hulklee1@users.noreply.github.com>
2021-03-24 20:32:09 +0900

    Add files via upload

---
+++ DBManager/DBManager/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿
+namespace DBManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sbPanel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.sbPanel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuMigration = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuDBOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            this.종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.executeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuExecSql = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSelSql = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.tbSql = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.mnuUpdate = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sbPanel1,
+            this.sbPanel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 444);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(631, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sbPanel1
+            // 
+            this.sbPanel1.Name = "sbPanel1";
+            this.sbPanel1.Size = new System.Drawing.Size(45, 17);
+            this.sbPanel1.Text = "DB File";
+            // 
+            // sbPanel2
+            // 
+            this.sbPanel2.Name = "sbPanel2";
+            this.sbPanel2.Size = new System.Drawing.Size(53, 17);
+            this.sbPanel2.Text = "Message";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.executeToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(631, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.mnuMigration,
+            this.toolStripMenuItem1,
+            this.saveAsToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.mnuDBOpen,
+            this.mnuUpdate,
+            this.toolStripMenuItem3,
+            this.종료ToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Enabled = false;
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // mnuMigration
+            // 
+            this.mnuMigration.Name = "mnuMigration";
+            this.mnuMigration.Size = new System.Drawing.Size(180, 22);
+            this.mnuMigration.Text = "Migration";
+            this.mnuMigration.Click += new System.EventHandler(this.mnuMigration_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Enabled = false;
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as...";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // mnuDBOpen
+            // 
+            this.mnuDBOpen.Name = "mnuDBOpen";
+            this.mnuDBOpen.Size = new System.Drawing.Size(180, 22);
+            this.mnuDBOpen.Text = "Database 열기";
+            this.mnuDBOpen.Click += new System.EventHandler(this.mnuDBOpen_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(177, 6);
+            // 
+            // 종료ToolStripMenuItem
+            // 
+            this.종료ToolStripMenuItem.Enabled = false;
+            this.종료ToolStripMenuItem.Name = "종료ToolStripMenuItem";
+            this.종료ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.종료ToolStripMenuItem.Text = "종료";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // executeToolStripMenuItem
+            // 
+            this.executeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuExecSql,
+            this.mnuSelSql});
+            this.executeToolStripMenuItem.Name = "executeToolStripMenuItem";
+            this.executeToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.executeToolStripMenuItem.Text = "Execute";
+            // 
+            // mnuExecSql
+            // 
+            this.mnuExecSql.Name = "mnuExecSql";
+            this.mnuExecSql.ShortcutKeys = System.Windows.Forms.Keys.F5;
+            this.mnuExecSql.Size = new System.Drawing.Size(228, 22);
+            this.mnuExecSql.Text = "Exec Sql";
+            this.mnuExecSql.Click += new System.EventHandler(this.mnuExecSql_Click);
+            // 
+            // mnuSelSql
+            // 
+            this.mnuSelSql.Name = "mnuSelSql";
+            this.mnuSelSql.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Shift | System.Windows.Forms.Keys.F5)));
+            this.mnuSelSql.Size = new System.Drawing.Size(228, 22);
+            this.mnuSelSql.Text = "exec Selected sql...";
+            this.mnuSelSql.Click += new System.EventHandler(this.mnuSelSql_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(3, 3);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowTemplate.Height = 23;
+            this.dataGrid.Size = new System.Drawing.Size(625, 233);
+            this.dataGrid.TabIndex = 2;
+            this.dataGrid.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dataGrid_CellBeginEdit);
+            // 
+            // tbSql
+            // 
+            this.tbSql.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSql.Location = new System.Drawing.Point(3, 3);
+            this.tbSql.Multiline = true;
+            this.tbSql.Name = "tbSql";
+            this.tbSql.Size = new System.Drawing.Size(625, 165);
+            this.tbSql.TabIndex = 3;
+            this.tbSql.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbSql_KeyDown);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.ValidateNames = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 27);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tbSql);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGrid);
+            this.splitContainer1.Size = new System.Drawing.Size(631, 414);
+            this.splitContainer1.SplitterDistance = 171;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // mnuUpdate
+            // 
+            this.mnuUpdate.Name = "mnuUpdate";
+            this.mnuUpdate.Size = new System.Drawing.Size(180, 22);
+            this.mnuUpdate.Text = "DB Update";
+            this.mnuUpdate.Click += new System.EventHandler(this.mnuUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 466);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "DBManager v1.2";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuMigration;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.TextBox tbSql;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem mnuDBOpen;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem 종료ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel sbPanel1;
+        private System.Windows.Forms.ToolStripStatusLabel sbPanel2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ToolStripMenuItem executeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuExecSql;
+        private System.Windows.Forms.ToolStripMenuItem mnuSelSql;
+        private System.Windows.Forms.ToolStripMenuItem mnuUpdate;
+    }
+}
+
---
+++ DBManager/DBManager/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuMigration_Click(object sender, EventArgs e)
+        {
+            DialogResult ret = openFileDialog1.ShowDialog();
+            if (ret != DialogResult.OK) return;
+            string nFile = openFileDialog1.FileName; // full name
+
+            StreamReader sr = new StreamReader(nFile);
+
+            // ==========================================================
+            //     Header 처리 프로세스
+            // =========================================
+            string buf = sr.ReadLine();     // 1 Line Read : Header Line
+            if (buf == null) return;
+            string[] sArr = buf.Split(',');
+            for(int i=0; i<sArr.Length;i++)
+            {
+                dataGrid.Columns.Add(sArr[i], sArr[i]);
+            }
+
+            // ==========================================================
+            //     Row 데이터 처리 프로세스
+            // =========================================
+            while(true)
+            {
+                buf = sr.ReadLine();     // 1 Line Read
+                if (buf == null) break;
+                sArr = buf.Split(',');
+                //dataGrid.Rows.Add(sArr);
+                int rIdx = dataGrid.Rows.Add();    // 1 Line 생성
+                for (int i = 0; i < sArr.Length; i++)
+                {
+                    dataGrid.Rows[rIdx].Cells[i].Value = sArr[i];
+                }
+            }
+        }
+
+        SqlConnection sqlCon = new SqlConnection();
+        SqlCommand sqlCmd = new SqlCommand();
+        string sConn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=;Integrated Security=True;Connect Timeout=30";
+        private void mnuDBOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult ret = openFileDialog1.ShowDialog();  // db file
+                if (ret != DialogResult.OK) return;
+                string nFile = openFileDialog1.FileName; // full name
+                string[] ss = sConn.Split(';');
+
+                sqlCmd.Connection = sqlCon;
+                sqlCon.ConnectionString = $"{ss[0]};{ss[1]}{nFile};{ss[2]};{ss[3]}";
+                sqlCon.Open();
+                sbPanel1.Text = openFileDialog1.SafeFileName;
+                sbPanel2.Text = "Database opened success.";
+                sbPanel1.BackColor = Color.Green;
+            }
+            catch(SqlException e1)
+            {
+                MessageBox.Show(e1.Message);
+                sbPanel2.Text = "Database cannnot open.";
+                sbPanel2.BackColor = Color.Red;
+            }
+        }
+        public string GetToken(int index, char deli, string str)
+        {
+            string[] Strs = str.Split(deli);
+            string ret = Strs[index];
+            return ret;
+        }
+
+        string TableName;  // 다른 메뉴에서 사용할 DB Table 이름. 현재 Open된 테이블
+        int RunSql(string s1)
+        {
+            try
+            {   // ex)  select * from fStatus
+                string sql = s1.Trim();
+                sqlCmd.CommandText = sql;   // insert into fstatus values ( 1,2,3,4)
+                if (GetToken(0, ' ', sql).ToUpper() == "SELECT")
+                {
+                    SqlDataReader sr = sqlCmd.ExecuteReader();
+
+                    TableName = GetToken(3, ' ', sql);
+                    dataGrid.Rows.Clear();
+                    dataGrid.Columns.Clear();
+
+                    for (int i = 0; i < sr.FieldCount; i++) // Header 처리
+                    {
+                        string ss = sr.GetName(i);
+                        dataGrid.Columns.Add(ss, ss);
+                    }
+                    for (int i=0;sr.Read();i++)  // 1 record read : 1 줄
+                    {
+                        int rIdx = dataGrid.Rows.Add();
+                        for(int j=0;j<sr.FieldCount;j++)
+                        {
+                            object str = sr.GetValue(j);
+                            dataGrid.Rows[rIdx].Cells[j].Value = str;
+                        }
+                    }
+                    sr.Close();
+
+                    //for (int i=0;sr.Read();i++)  // 1 record read : 1 줄
+                    //{
+                    //    string buf = "";
+                    //    for(int j=0;j<sr.FieldCount;j++)
+                    //    {
+                    //        object str = sr.GetValue(j);
+                    //        buf += $" {str} ";
+                    //    }
+                    //    tbSql.Text += $"\r\n{buf}";
+                    //}
+                    //sr.Close();
+                }
+                else
+                {
+                    sqlCmd.ExecuteNonQuery();   // select 문 제외 -- no return value
+                                                // update, insert, delete, create, alt
+                }
+                sbPanel2.Text = "Success";
+                sbPanel2.BackColor = Color.AliceBlue;
+            }
+            catch (SqlException e1)
+            {
+                MessageBox.Show(e1.Message);
+                sbPanel2.Text = "Error";
+                sbPanel2.BackColor = Color.Red;
+
+            }
+            catch (InvalidOperationException e2)
+            { 
+                MessageBox.Show(e2.Message);
+                sbPanel2.Text = "Error";
+                sbPanel2.BackColor = Color.Red;
+            }
+            return 0;
+        }
+        private void mnuExecSql_Click(object sender, EventArgs e)
+        {
+            RunSql(tbSql.Text);
+        }
+
+        private void mnuSelSql_Click(object sender, EventArgs e)
+        {
+            RunSql(tbSql.SelectedText);
+        }
+
+        private void tbSql_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter) return;
+
+            string str = tbSql.Text;
+            string[] sArr = str.Split('\n');    // 줄바꿈문자 : \r\n
+            int n = sArr.Length;
+            string sql = sArr[n - 1].Trim();
+            RunSql(sql);
+        }
+
+        private void dataGrid_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            dataGrid.Rows[e.RowIndex].Cells[e.ColumnIndex].ToolTipText = ".";
+        }
+
+        private void mnuUpdate_Click(object sender, EventArgs e)
+        {
+            for(int i=0;i<dataGrid.Rows.Count;i++)
+            {
+                for(int j=0;j<dataGrid.Columns.Count;j++)
+                {
+                    string s = dataGrid.Rows[i].Cells[j].ToolTipText;
+                    if(s == ".")
+ // update [Table]   set [field]=(CellText) where [keyName]=(Key.CellText)
+ // update [fStatus] set [Temp] =(10)       where [ID]     =(6)
+                    {
+                        string tn = TableName;
+                        string fn = dataGrid.Columns[j].HeaderText;
+                        string ct = (string)dataGrid.Rows[i].Cells[j].Value;
+                        string kn = dataGrid.Columns[0].HeaderText;
+                        int kt = (int)dataGrid.Rows[i].Cells[0].Value;
+                        string sql = $"update {tn} set {fn}={ct} where {kn}={kt}";
+                        RunSql(sql);
+                    }
+                }
+            }
+        }
+    }
+}
