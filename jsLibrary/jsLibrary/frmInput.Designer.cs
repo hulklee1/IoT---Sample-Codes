c53b06496e54d0e6551f6292b7598c075c2257c3
hulklee1 <35159494+hulklee1@users.noreply.github.com>
2021-04-28 15:48:17 +0900

    Add files via upload

---
+++ jsLibrary/jsLibrary/frmInput.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace jsLibrary
+{
+    partial class frmInput
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.lblCaption = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(3, 15);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(173, 21);
+            this.tbInput.TabIndex = 0;
+            this.tbInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbInput_KeyDown);
+            // 
+            // lblCaption
+            // 
+            this.lblCaption.AutoSize = true;
+            this.lblCaption.Location = new System.Drawing.Point(3, 0);
+            this.lblCaption.Name = "lblCaption";
+            this.lblCaption.Size = new System.Drawing.Size(32, 12);
+            this.lblCaption.TabIndex = 1;
+            this.lblCaption.Text = "Input";
+            // 
+            // frmInput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(180, 44);
+            this.Controls.Add(this.lblCaption);
+            this.Controls.Add(this.tbInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmInput";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label lblCaption;
+    }
+}
---
+++ jsLibrary/jsLibrary/frmInput.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Net.Sockets;
+using System.Data.SqlClient;
+
+namespace jsLibrary
+{
+    public partial class frmInput: Form
+    {   // frmInput dlg = new frmInput("입력하세요");
+        // if(dlg.DoModal() == OK) str = dlg.retString;
+        public frmInput(string cap)
+        {
+            InitializeComponent();
+            lblCaption.Text = cap;
+        }
+        public string retString = "";
+
+        private void tbInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                retString = tbInput.Text;
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            else if(e.KeyCode == Keys.Escape)
+            {
+                DialogResult = DialogResult.Cancel;
+                Close();
+            }
+        }
+    }
+    public class iniFile
+    {
+        [DllImport("kernel32")]
+        static extern int GetPrivateProfileString(string s, string k, string d, StringBuilder sb, int n, string p);
+        [DllImport("kernel32")]
+        static extern int WritePrivateProfileString(string s, string k, string v, string p);
+
+        public string iniFilePath = "";
+        public iniFile(string path)
+        {
+            iniFilePath = path;
+        }
+        public string GetString(string s, string k, string d = "")
+        {
+            StringBuilder sb = new StringBuilder(512);
+            GetPrivateProfileString(s, k, d, sb, 512, iniFilePath);
+            return sb.ToString();
+        }
+        public int SetString(string s, string k, string v)
+        {
+            return WritePrivateProfileString(s, k, v, iniFilePath);
+        }
+    }
+    public class SqlDatabase
+    {
+        string ConnectionString = null;
+        SqlConnection sqlConn = null;
+        SqlCommand sqlCmd = null;
+        public SqlDatabase(string connStr)
+        {
+            ConnectionString = connStr;
+            sqlConn = new SqlConnection(ConnectionString);
+            sqlConn.Open();
+            sqlCmd = new SqlCommand("", sqlConn);
+        }
+        public object run(string sql)
+        {
+            sqlCmd.CommandText = sql;   // "  select count(*) from fstatus    "
+            if (jslib.GetToken(0, sql.Trim(), ' ').ToUpper() == "SELECT") // "[SELECT] count(*) from fstatus"
+            {
+                SqlDataReader sr = sqlCmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(sr);
+                sr.Close();
+
+                return dt;
+            }
+            else
+            {
+                return sqlCmd.ExecuteNonQuery();  // insert, update, delete, create, alter 등 조회결과가 없는 SQL문 처리
+            }
+        }
+        public object runScalar(string sql)
+        {
+            try
+            {
+                sqlCmd.CommandText = sql;   // "  select count(*) from fstatus    "
+                return sqlCmd.ExecuteScalar();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return null;
+            }
+        }
+    }
+    public static class jslib
+    {
+        public static string GetToken(int n, string str, char d)
+        {
+            string[] sArr = str.Split(d);
+            return sArr[n];
+        }
+
+        public static void WriteLog(string str)
+        {
+            StreamWriter sw = new StreamWriter(@".\errLog.log", true);
+            sw.Write($"{DateTime.Now}:{str}\r\n");
+            sw.Close();
+        }
+
+        public static string GetInput(string str = "Input")
+        {
+            frmInput dlg = new frmInput(str);
+            if (dlg.ShowDialog() == DialogResult.OK)
+                return dlg.retString;
+            else return "";
+        }
+
+        public static bool IsAlive(Socket ss) // 소켓 ss 가 현재 유효한지 아닌지 판단
+        {
+            if (ss == null) return false;
+            if (!ss.Connected) return false;
+            if (ss.Poll(1000, SelectMode.SelectRead) && ss.Available == 0) return false;  // ???
+            try
+            {
+                ss.Send(new byte[1], 0, SocketFlags.OutOfBand);  // 실제 전송되는 데이터 바이트는 0
+                return true;
+            }
+            catch { return false; }
+        }
+    }
+    public class SQLDB
+    {
+        //  클래스 변수 정의
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlCmd = new SqlCommand();
+        string ConnStr;
+
+        //  클래스 생성자 정의 - 클래스 명과 동일
+        public SQLDB(string s) // s : connection string 연결 문자열
+        {
+            ConnStr = s;
+            sqlConn.ConnectionString = ConnStr;
+            sqlConn.Open();
+            sqlCmd.Connection = sqlConn;
+        }
+
+        //  클래스 함수(메쏘드) 정의 
+        public object Run(string sql)
+        {
+            try
+            {
+                sqlCmd.CommandText = sql;   // "  select * from fstatus    "
+                if (jslib.GetToken(0, sql.Trim(), ' ').ToUpper() == "SELECT") // "[SELECT] count(*) from fstatus"
+                {
+                    SqlDataReader sr = sqlCmd.ExecuteReader();
+                    DataTable dt = new DataTable();
+                    dt.Load(sr);
+                    sr.Close();
+
+                    return dt;
+                }
+                else
+                {
+                    return sqlCmd.ExecuteNonQuery();  // insert, update, delete, create, alter 등 조회결과가 없는 SQL문 처리
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return null;
+            }
+        }
+        public object Get(string sql)   // 단일 필드 데이터 반환
+        {
+            try
+            {
+                sqlCmd.CommandText = sql;   // "  select count(*) from fstatus    "
+                if (jslib.GetToken(0, sql.Trim(), ' ').ToUpper() == "SELECT") // "[SELECT] count(*) from fstatus"
+                {
+                    return sqlCmd.ExecuteScalar();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return null;
+        }
+    }
+}
