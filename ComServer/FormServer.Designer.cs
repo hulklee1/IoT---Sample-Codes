96791fc445bc180931ea6626e087ba9972ede24e
hulklee1 <35159494+hulklee1@users.noreply.github.com>
2021-05-26 10:27:41 +0900

    Add files via upload

---
+++ ComServer/FormServer.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace ComTest
+{
+    partial class FormServer
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sbServerMessage = new System.Windows.Forms.ToolStripStatusLabel();
+            this.sbServerLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.tbServer = new System.Windows.Forms.TextBox();
+            this.tbClient = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbServerPort = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbIP = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(836, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sbServerMessage,
+            this.sbServerLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 482);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(836, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sbServerMessage
+            // 
+            this.sbServerMessage.AutoSize = false;
+            this.sbServerMessage.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.sbServerMessage.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner;
+            this.sbServerMessage.Name = "sbServerMessage";
+            this.sbServerMessage.Size = new System.Drawing.Size(200, 17);
+            // 
+            // sbServerLabel2
+            // 
+            this.sbServerLabel2.AutoSize = false;
+            this.sbServerLabel2.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.sbServerLabel2.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner;
+            this.sbServerLabel2.Name = "sbServerLabel2";
+            this.sbServerLabel2.Size = new System.Drawing.Size(300, 17);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnStop);
+            this.splitContainer1.Panel2.Controls.Add(this.btnStart);
+            this.splitContainer1.Panel2.Controls.Add(this.tbServerPort);
+            this.splitContainer1.Panel2.Controls.Add(this.btnSend);
+            this.splitContainer1.Panel2.Controls.Add(this.tbPort);
+            this.splitContainer1.Panel2.Controls.Add(this.tbIP);
+            this.splitContainer1.Size = new System.Drawing.Size(836, 458);
+            this.splitContainer1.SplitterDistance = 590;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.tbServer);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.tbClient);
+            this.splitContainer2.Size = new System.Drawing.Size(590, 458);
+            this.splitContainer2.SplitterDistance = 218;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // tbServer
+            // 
+            this.tbServer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbServer.Location = new System.Drawing.Point(3, 3);
+            this.tbServer.Multiline = true;
+            this.tbServer.Name = "tbServer";
+            this.tbServer.Size = new System.Drawing.Size(584, 212);
+            this.tbServer.TabIndex = 0;
+            // 
+            // tbClient
+            // 
+            this.tbClient.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbClient.Location = new System.Drawing.Point(3, 3);
+            this.tbClient.Multiline = true;
+            this.tbClient.Name = "tbClient";
+            this.tbClient.Size = new System.Drawing.Size(584, 230);
+            this.tbClient.TabIndex = 1;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(35, 63);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(109, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(35, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(109, 25);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Server Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbServerPort
+            // 
+            this.tbServerPort.Location = new System.Drawing.Point(150, 15);
+            this.tbServerPort.Name = "tbServerPort";
+            this.tbServerPort.Size = new System.Drawing.Size(37, 21);
+            this.tbServerPort.TabIndex = 0;
+            this.tbServerPort.Text = "9001";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(35, 254);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(109, 25);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send 1 Packet";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(107, 227);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(37, 21);
+            this.tbPort.TabIndex = 0;
+            this.tbPort.Text = "9001";
+            // 
+            // tbIP
+            // 
+            this.tbIP.Location = new System.Drawing.Point(35, 227);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.Size = new System.Drawing.Size(73, 21);
+            this.tbIP.TabIndex = 0;
+            this.tbIP.Text = "127.0.0.1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FormServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 504);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormServer";
+            this.Text = "Tcp Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormServer_FormClosing);
+            this.Load += new System.EventHandler(this.FormServer_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TextBox tbServer;
+        private System.Windows.Forms.TextBox tbClient;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbServerPort;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbIP;
+        private System.Windows.Forms.ToolStripStatusLabel sbServerMessage;
+        private System.Windows.Forms.ToolStripStatusLabel sbServerLabel2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ ComServer/FormServer.cs
@@ -0,0 +1,164 @@
+﻿using myLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComTest
+{
+    public partial class FormServer : Form
+    {
+        public FormServer()
+        {
+            InitializeComponent();
+        }
+
+        delegate void cbAddText(string str);  // string str을 인수로 받아서 tbServer에 출력하는 콜백 함수
+        //    invoke
+        void AddText(string str)    // Thread 내부에서 호출될 함수  tbServer 출력
+        {
+            if(tbServer.InvokeRequired)
+            {
+                cbAddText at = new cbAddText(AddText);
+                object[] oArr = { str };
+                Invoke(at, oArr);
+            }
+            else
+            {
+                tbServer.Text += str;   // Thread에서 수행해야 할 본래의 작업
+            }
+        }
+
+        Thread thread = null;
+        TcpListener listener = null;
+        string mainMsg = "";
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+          //  ServerProcess();
+            if (thread == null)
+            {
+                thread = new Thread(ServerProcess);
+                thread.Start();
+            }
+            timer1.Start();
+            sbServerMessage.Text = "Listener started..";
+            AddText($"ServerProcess now started... Open Port is {tbServerPort}.");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            thread.Abort(); // thread.Suspend(); thread.Resume();
+            thread = null;
+            timer1.Stop();
+            sbServerMessage.Text = "Server stopped..";
+        }
+        Socket sock = null;
+        void ServerProcess()
+        {
+            while(true)
+            {
+                if (listener == null)
+                {
+                    listener = new TcpListener(int.Parse(tbServerPort.Text));
+                    listener.Start();
+                }
+                if (listener.Pending())  // Non-Blocking Method
+                {
+                    //TcpClient tcp = listener.AcceptTcpClient();     // Blocking Method
+
+                    //EndPoint ep = tcp.Client.RemoteEndPoint;
+                    //sbServerLabel2.Text = ep.ToString(); // xxx.xxx.xxx.xxx  :xxxxx
+                    //NetworkStream ns = tcp.GetStream();
+                    //byte[] bArr = new byte[200];
+                    //while(ns.DataAvailable)
+                    //{
+                    //    int n = ns.Read(bArr, 0, 200);
+                    //    //tbServer.Text += Encoding.Default.GetString(bArr,0,n);  // Cross-thread error
+                    //    //mainMsg += Encoding.Default.GetString(bArr,0,n);  // Cross-thread error
+                    //    AddText(Encoding.Default.GetString(bArr, 0, n));
+                    //}
+                    if (sock != null) sock.Close();
+                    sock = listener.AcceptSocket();    // Mission : TcpClient 가 아닌 Socket 을 이용해서 Accept 및 입력 stream 처리
+                    ////////byte[] bArr = new byte[sock.Available];
+                    ////////int n = sock.Receive(bArr);
+                    ////////AddText(Encoding.Default.GetString(bArr, 0, n));
+
+                    ////////sock.Send(Encoding.Default.GetBytes("OK"));
+
+                    //IPEndPoint ep = (IPEndPoint)sock.RemoteEndPoint;  // 127.0.0.1:76543
+                    //sbServerLabel2.Text = $"{ep.Address}:{ep.Port}";  // xxx.xxx.xxx.xxx  :xxxxx
+
+                    sbServerLabel2.Text = $"{  mylib.GetToken(0, sock.RemoteEndPoint.ToString(), ':')}";  // xxx.xxx.xxx.xxx  :xxxxx
+                    int j = mylib.Add(1, 2);
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)  //     1/1000 초 단위로 tick 발생 
+        {
+            timer1.Stop();
+            if(sock != null)
+            {
+                int n = sock.Available;
+                if(n>0)
+                {
+                    byte[] bArr = new byte[n];
+                    int m = sock.Receive(bArr);
+                    AddText(Encoding.Default.GetString(bArr, 0, m));
+                }
+            }
+            //////tbServer.Text += mainMsg;
+            //////mainMsg = "";
+            timer1.Start();
+        }
+
+        iniFile ini;
+        private void FormServer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ini.SetString("Form", "LocX",$"{Location.X}");
+            ini.SetString("Form", "LocY",$"{Location.Y}");
+
+            ini.SetString("Form", "SizeX", $"{Size.Width}");
+            ini.SetString("Form", "SizeY", $"{Size.Height}");
+
+            ini.SetString("Form", "Splitter", $"{splitContainer1.SplitterDistance}");
+
+            if(thread != null)
+                thread.Abort(); // Thread 종료
+        }
+
+        private void FormServer_Load(object sender, EventArgs e)
+        {
+            int x1, y1, x2, y2;
+
+            ini = new iniFile("");
+
+            ini.ChangeFileName(".\\ComServer.ini");
+
+            x1 = int.Parse(ini.GetString("Form", "LocX","0"));
+            y1 = int.Parse(ini.GetString("Form", "LocY","0"));
+            Location = new Point(x1, y1);
+
+            x2 = int.Parse(ini.GetString("Form", "SizeX", "500"));
+            y2 = int.Parse(ini.GetString("Form", "SizeY", "500"));
+            Size = new Size(x2, y2);
+
+            splitContainer1.SplitterDistance = int.Parse(ini.GetString("Form", "Splitter", "300"));
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+            sock.Send(Encoding.Default.GetBytes(tbClient.Text));
+        }
+    }
+}
