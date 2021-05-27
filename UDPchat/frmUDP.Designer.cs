b830369eca3cb81181967e0e7324690739f0657e
hulklee1 <35159494+hulklee1@users.noreply.github.com>
2021-05-27 16:36:08 +0900

    Add files via upload

---
+++ UDPchat/frmUDP.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace chatUDP
+{
+    partial class frmUDP
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sbLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.sbLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbMemo = new System.Windows.Forms.TextBox();
+            this.tbSend = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuSend = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.sbLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.sbLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sbLabel1,
+            this.sbLabel2,
+            this.toolStripStatusLabel1,
+            this.sbLabel3,
+            this.sbLabel4});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 438);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(478, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sbLabel1
+            // 
+            this.sbLabel1.AutoSize = false;
+            this.sbLabel1.DoubleClickEnabled = true;
+            this.sbLabel1.Name = "sbLabel1";
+            this.sbLabel1.Size = new System.Drawing.Size(100, 17);
+            this.sbLabel1.Text = "127.0.0.1";
+            this.sbLabel1.DoubleClick += new System.EventHandler(this.sbLabel1_DoubleClick);
+            // 
+            // sbLabel2
+            // 
+            this.sbLabel2.AutoSize = false;
+            this.sbLabel2.DoubleClickEnabled = true;
+            this.sbLabel2.Name = "sbLabel2";
+            this.sbLabel2.Size = new System.Drawing.Size(50, 17);
+            this.sbLabel2.Text = "9100";
+            this.sbLabel2.DoubleClick += new System.EventHandler(this.sbLabel2_DoubleClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuStart});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(478, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuStart
+            // 
+            this.mnuStart.Name = "mnuStart";
+            this.mnuStart.Size = new System.Drawing.Size(44, 20);
+            this.mnuStart.Text = "Start";
+            this.mnuStart.Click += new System.EventHandler(this.mnuStart_Click);
+            // 
+            // tbMemo
+            // 
+            this.tbMemo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbMemo.Location = new System.Drawing.Point(0, 27);
+            this.tbMemo.Multiline = true;
+            this.tbMemo.Name = "tbMemo";
+            this.tbMemo.Size = new System.Drawing.Size(478, 290);
+            this.tbMemo.TabIndex = 2;
+            // 
+            // tbSend
+            // 
+            this.tbSend.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSend.ContextMenuStrip = this.contextMenuStrip1;
+            this.tbSend.Location = new System.Drawing.Point(0, 323);
+            this.tbSend.Multiline = true;
+            this.tbSend.Name = "tbSend";
+            this.tbSend.Size = new System.Drawing.Size(478, 112);
+            this.tbSend.TabIndex = 3;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuSend});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(102, 26);
+            // 
+            // mnuSend
+            // 
+            this.mnuSend.Name = "mnuSend";
+            this.mnuSend.Size = new System.Drawing.Size(101, 22);
+            this.mnuSend.Text = "Send";
+            this.mnuSend.Click += new System.EventHandler(this.mnuSend_Click);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.AutoSize = false;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(121, 17);
+            // 
+            // sbLabel3
+            // 
+            this.sbLabel3.AutoSize = false;
+            this.sbLabel3.DoubleClickEnabled = true;
+            this.sbLabel3.Name = "sbLabel3";
+            this.sbLabel3.Size = new System.Drawing.Size(100, 17);
+            this.sbLabel3.Text = "127.0.0.1";
+            this.sbLabel3.DoubleClick += new System.EventHandler(this.sbLabel3_DoubleClick);
+            // 
+            // sbLabel4
+            // 
+            this.sbLabel4.AutoSize = false;
+            this.sbLabel4.DoubleClickEnabled = true;
+            this.sbLabel4.Name = "sbLabel4";
+            this.sbLabel4.Size = new System.Drawing.Size(50, 17);
+            this.sbLabel4.Text = "9100";
+            this.sbLabel4.DoubleClick += new System.EventHandler(this.sbLabel4_DoubleClick);
+            // 
+            // frmUDP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 460);
+            this.Controls.Add(this.tbSend);
+            this.Controls.Add(this.tbMemo);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmUDP";
+            this.Text = "UDP Test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmUDP_FormClosing);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel sbLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel sbLabel2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuStart;
+        private System.Windows.Forms.TextBox tbMemo;
+        private System.Windows.Forms.TextBox tbSend;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuSend;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel sbLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel sbLabel4;
+    }
+}
+
---
+++ UDPchat/frmUDP.cs
@@ -0,0 +1,104 @@
+﻿using jsLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chatUDP
+{
+    public partial class frmUDP : Form
+    {
+        public frmUDP()
+        {
+            InitializeComponent();
+        }
+
+        Socket udpServer = null;
+        Thread threadRead = null;
+        private void mnuStart_Click(object sender, EventArgs e)
+        {   // 1. socket 생성     2. EndPoint 구성      3. Binding         (4. Listening xxx)
+            if(udpServer == null)
+            {
+                udpServer = new Socket(AddressFamily.InterNetwork,SocketType.Dgram,ProtocolType.Udp);
+            }
+            //IPEndPoint ep = new IPEndPoint(IPAddress.Parse(sbLabel3.Text), int.Parse(sbLabel4.Text)); // 특정 주소 (sbLabel3)로 들어오는 메시지 
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, int.Parse(sbLabel2.Text)); // sbLabel2 (개방포트)로 들어오는 모든 메시지 
+            udpServer.Bind(ep);
+
+            if(threadRead == null)
+            {
+                threadRead = new Thread(ReadProcess);
+                threadRead.Start();
+            }
+        }
+
+        delegate void cbAdd(string s);
+        void AddText(string str)
+        {
+            if(tbMemo.InvokeRequired)
+            {
+                cbAdd cb = new cbAdd(AddText);
+                Invoke(cb, new object[] { str });
+            }
+            else
+            {
+                tbMemo.AppendText(str);
+            }
+        }
+
+        void ReadProcess()
+        {
+            while(true)
+            {
+                if(udpServer.Available > 0)
+                {
+                    byte[] bArr = new byte[udpServer.Available];
+                    udpServer.Receive(bArr);
+                    AddText(Encoding.Default.GetString(bArr));
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        private void sbLabel1_DoubleClick(object sender, EventArgs e)
+        {
+            sbLabel1.Text = jslib.GetInput("IP Address");
+        }
+        private void sbLabel2_DoubleClick(object sender, EventArgs e) // Port change
+        {
+            sbLabel2.Text = jslib.GetInput("Port");
+        }
+        private void sbLabel3_DoubleClick(object sender, EventArgs e)
+        {
+            sbLabel3.Text = jslib.GetInput("IP Address");
+        }
+        private void sbLabel4_DoubleClick(object sender, EventArgs e)
+        {
+            sbLabel4.Text = jslib.GetInput("Port");
+        }
+
+        void UDPSend(string str)  // UDP 송신 : 1.Socket(1회성) 생성  2.EndPoint 구성   3.Send 끝
+        {
+            Socket lsock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(sbLabel1.Text), int.Parse(sbLabel2.Text));
+            lsock.SendTo(Encoding.Default.GetBytes(str), ep);
+        }
+        private void mnuSend_Click(object sender, EventArgs e)  // UDP 송신 : 1.Socket(1회성) 생성  2.EndPoint 구성   3.Send 끝
+        {
+            UDPSend(tbSend.Text);
+        }
+
+        private void frmUDP_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(threadRead != null)  threadRead.Abort();
+        }
+    }
+}
