594a052bc7c0ab3f35a22aec010f269cd4ccb585
hulklee1 <35159494+hulklee1@users.noreply.github.com>
2021-10-12 17:03:58 +0900

    Add files via upload

---
+++ IoT216-22/ChatClient/frmClient.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace ChatManager
+{
+    partial class frmClient
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sbLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.sbLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbSend = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbReceive = new System.Windows.Forms.TextBox();
+            this.brnSend = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbIP = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(726, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sbLabel1,
+            this.sbLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(726, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sbLabel1
+            // 
+            this.sbLabel1.AutoSize = false;
+            this.sbLabel1.Name = "sbLabel1";
+            this.sbLabel1.Size = new System.Drawing.Size(100, 17);
+            // 
+            // sbLabel2
+            // 
+            this.sbLabel2.AutoSize = false;
+            this.sbLabel2.Name = "sbLabel2";
+            this.sbLabel2.Size = new System.Drawing.Size(50, 17);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Panel2.Controls.Add(this.brnSend);
+            this.splitContainer1.Panel2.Controls.Add(this.btnConnect);
+            this.splitContainer1.Panel2.Controls.Add(this.tbPort);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.tbIP);
+            this.splitContainer1.Size = new System.Drawing.Size(726, 404);
+            this.splitContainer1.SplitterDistance = 527;
+            this.splitContainer1.TabIndex = 3;
+            this.splitContainer1.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer1_SplitterMoved);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.label4);
+            this.splitContainer2.Panel1.Controls.Add(this.tbSend);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.label3);
+            this.splitContainer2.Panel2.Controls.Add(this.tbReceive);
+            this.splitContainer2.Size = new System.Drawing.Size(527, 404);
+            this.splitContainer2.SplitterDistance = 260;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(3, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "[ Send ]";
+            // 
+            // tbSend
+            // 
+            this.tbSend.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSend.Location = new System.Drawing.Point(3, 30);
+            this.tbSend.Multiline = true;
+            this.tbSend.Name = "tbSend";
+            this.tbSend.Size = new System.Drawing.Size(254, 371);
+            this.tbSend.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(3, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "[ Receive ]";
+            // 
+            // tbReceive
+            // 
+            this.tbReceive.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbReceive.Location = new System.Drawing.Point(-1, 30);
+            this.tbReceive.Multiline = true;
+            this.tbReceive.Name = "tbReceive";
+            this.tbReceive.Size = new System.Drawing.Size(261, 371);
+            this.tbReceive.TabIndex = 0;
+            // 
+            // brnSend
+            // 
+            this.brnSend.Location = new System.Drawing.Point(62, 156);
+            this.brnSend.Name = "brnSend";
+            this.brnSend.Size = new System.Drawing.Size(120, 41);
+            this.brnSend.TabIndex = 4;
+            this.brnSend.Text = "Send";
+            this.brnSend.UseVisualStyleBackColor = true;
+            this.brnSend.Click += new System.EventHandler(this.brnSend_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(62, 56);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(121, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(147, 29);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(36, 21);
+            this.tbPort.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server";
+            // 
+            // tbIP
+            // 
+            this.tbIP.Location = new System.Drawing.Point(62, 29);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.Size = new System.Drawing.Size(86, 21);
+            this.tbIP.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 298);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 6;
+            // 
+            // frmClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmClient";
+            this.Text = "CharClinet";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmClient_FormClosing);
+            this.Load += new System.EventHandler(this.frmClient_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TextBox tbSend;
+        private System.Windows.Forms.TextBox tbReceive;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbIP;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button brnSend;
+        private System.Windows.Forms.ToolStripStatusLabel sbLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel sbLabel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ IoT216-22/ChatClient/frmClient.cs
@@ -0,0 +1,175 @@
+﻿using jsLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatManager
+{
+    public partial class frmClient : Form
+    {
+        public frmClient()
+        {
+            InitializeComponent();
+        }
+
+        delegate void cbAddText(string s);
+        void AddText(string str)
+        {
+            if(tbReceive.InvokeRequired)
+            {
+                cbAddText cb = new cbAddText(AddText);
+                object[] oArr = { str };
+                Invoke(cb, oArr);
+                //Invoke(cb, new object[] { str });
+            }
+            else
+            {
+                tbReceive.AppendText(str) ;  // byte[]의 Size 만큼 변환 (NULL 포함)
+            }
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+            int size1 = 200; // splitContainer1.Panel2 의 사이즈
+
+            splitContainer1.SplitterDistance = splitContainer1.Size.Width - size1;
+        }
+
+        Thread threadClient = null;
+        Socket sock = null;        
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(sock == null)
+                {
+                    sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                }
+                else
+                {
+                    if(threadClient != null)
+                    {
+                        threadClient.Abort();
+                        threadClient = null;
+                    }
+                    sock.Close();
+                    //sock = null;
+                    sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                }
+                sock.Connect(tbIP.Text, int.Parse(tbPort.Text));    // Blocking mode : TimeOut 발생 전까지 (2~4초)
+                //Thread ConThread = new Thread(ConnectProcess);
+                //ConThread.Start();
+                //while (true)
+                //{
+                //    if (!ConThread.IsAlive) break;
+                //}
+
+                sbLabel1.Text = ((IPEndPoint)(sock.RemoteEndPoint)).Address.ToString();
+                sbLabel2.Text = ((IPEndPoint)(sock.RemoteEndPoint)).Port.ToString();
+
+                if(threadClient == null)
+                {
+                    threadClient = new Thread(ClientProcess);
+                    threadClient.Start();
+                }
+            }
+            catch(Exception e1)
+            {
+                tbReceive.AppendText(e1.Message + "\r\n");
+            }
+        }
+
+        void ConnectProcess()
+        {
+            sock.Connect(tbIP.Text, int.Parse(tbPort.Text));
+            return;
+        }
+
+        void ClientProcess()    // Thread 등록 프로세스
+        {
+            while(true)
+            {
+                int n = sock.Available;
+                if(n>0 && sock.Connected)
+                {
+                    byte[] bArr = new byte[n];
+                    sock.Receive(bArr);  // c#에서의 통신은 byte[] - Byte Array :      c/c++ char
+                    AddText(Encoding.Default.GetString(bArr));
+                    //tbReceive.AppendText(Encoding.Default.GetString(bArr)) ;  // byte[]의 Size 만큼 변환 (NULL 포함)
+                }
+            }
+        }
+
+        private void brnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(sock.Connected == true)
+                {
+                    string str = tbSend.Text.Trim();  // multi line  \r\n CRLF
+                    string[] sArr = str.Split('\r');
+                    string sLast = sArr[sArr.Length - 1];
+                    sock.Send(Encoding.Default.GetBytes(sLast));
+                }
+                else
+                {
+                    AddText("서버와의 통신이 원활하지 않습니다.\r\n 연결을 점검하고 다시 시도하세요\r\n");
+                }
+            }
+            catch(Exception e1)
+            {
+                AddText(e1.Message);
+            }
+        }
+
+        iniFile ini = new iniFile(".\\ChatClient.ini");
+        private void frmClient_Load(object sender, EventArgs e)
+        {
+            int x1, x2, y1, y2;
+            tbIP.Text = ini.GetString("Server", "IP", "127.0.0.1");
+            tbPort.Text = ini.GetString("Server", "Port", "9001");
+
+            x1 = int.Parse(ini.GetString("Form", "LocationX", "0"));
+            y1 = int.Parse(ini.GetString("Form", "LocationY", "0"));
+            this.Location = new Point(x1, y1);
+
+            x2 = int.Parse(ini.GetString("Form", "SizeX", "500"));
+            y2 = int.Parse(ini.GetString("Form", "SizeY", "500"));
+            this.Size = new Size(x2, y2);
+        }
+
+        private void frmClient_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ini.SetString("Server", "IP", tbIP.Text );    
+            ini.SetString("Server", "Port", tbPort.Text); 
+            ini.SetString("Form", "LocationX", $"{Location.X}");
+            ini.SetString("Form", "LocationY", $"{Location.Y}");
+            ini.SetString("Form", "SizeX", $"{Size.Width}");   
+            ini.SetString("Form", "SizeY", $"{Size.Height}");
+
+            if (threadClient != null)
+                threadClient.Abort();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s1 = "123";
+            string s2 = "456123";
+            string s3 = "789123";
+            int s4 = int.Parse(textBox1.Text);
+
+            string str = $"s1:{s1,-05}    s2:{s2,07}   s3:{s3,7}   s4:{s4,-10:D7}";
+            AddText(str);
+        }
+    }
+}
