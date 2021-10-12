9873a2bd1dc42332ad61385fee4014887cda74d5
hulklee1 <35159494+hulklee1@users.noreply.github.com>
2021-10-12 17:03:19 +0900

    Add files via upload

---
+++ IoT216-22/ChatServer/frmServer.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace ChatServer
+{
+    partial class frmServer
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sbLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.tbSend = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuSend = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbReceive = new System.Windows.Forms.TextBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sbLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 437);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(778, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sbLabel1
+            // 
+            this.sbLabel1.AutoSize = false;
+            this.sbLabel1.Name = "sbLabel1";
+            this.sbLabel1.Size = new System.Drawing.Size(120, 17);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(778, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.radioButton3);
+            this.splitContainer1.Panel2.Controls.Add(this.radioButton2);
+            this.splitContainer1.Panel2.Controls.Add(this.radioButton1);
+            this.splitContainer1.Panel2.Controls.Add(this.checkBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.btnSend);
+            this.splitContainer1.Panel2.Controls.Add(this.btnStart);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.tbPort);
+            this.splitContainer1.Size = new System.Drawing.Size(778, 413);
+            this.splitContainer1.SplitterDistance = 580;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.tbSend);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.tbReceive);
+            this.splitContainer2.Size = new System.Drawing.Size(580, 413);
+            this.splitContainer2.SplitterDistance = 274;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // tbSend
+            // 
+            this.tbSend.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSend.ContextMenuStrip = this.contextMenuStrip1;
+            this.tbSend.Location = new System.Drawing.Point(3, 3);
+            this.tbSend.Multiline = true;
+            this.tbSend.Name = "tbSend";
+            this.tbSend.Size = new System.Drawing.Size(268, 404);
+            this.tbSend.TabIndex = 0;
+            this.tbSend.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbSend_KeyUp);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuSend});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(102, 26);
+            // 
+            // mnuSend
+            // 
+            this.mnuSend.Name = "mnuSend";
+            this.mnuSend.Size = new System.Drawing.Size(101, 22);
+            this.mnuSend.Text = "Send";
+            this.mnuSend.Click += new System.EventHandler(this.mnuSend_Click);
+            // 
+            // tbReceive
+            // 
+            this.tbReceive.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbReceive.Location = new System.Drawing.Point(3, 6);
+            this.tbReceive.Multiline = true;
+            this.tbReceive.Name = "tbReceive";
+            this.tbReceive.Size = new System.Drawing.Size(296, 401);
+            this.tbReceive.TabIndex = 0;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(44, 324);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(92, 16);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton1";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(44, 302);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(92, 16);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton1";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(44, 346);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(92, 16);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(44, 237);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(86, 16);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(25, 81);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(133, 54);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(25, 52);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(134, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server Port";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(86, 25);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(73, 21);
+            this.tbPort.TabIndex = 0;
+            // 
+            // frmServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 459);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmServer";
+            this.Text = "ChatServer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmServer_FormClosing);
+            this.Load += new System.EventHandler(this.frmServer_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.ToolStripStatusLabel sbLabel1;
+        private System.Windows.Forms.TextBox tbSend;
+        private System.Windows.Forms.TextBox tbReceive;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuSend;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ IoT216-22/ChatServer/frmServer.cs
@@ -0,0 +1,154 @@
+﻿using jsLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatServer
+{
+    public partial class frmServer : Form
+    {
+        public frmServer()
+        {
+            InitializeComponent();
+        }
+
+        delegate void CB(string str);
+        void AddText(string str)
+        {
+            if(tbReceive.InvokeRequired)
+            {
+                CB cb = new CB(AddText);
+                Invoke(cb, new object[] { str });
+            }
+            else
+            {
+                tbReceive.AppendText(str);
+            }
+        }
+
+        string TmpString = "";
+        Thread threadServer = null;
+        Thread threadRead = null;
+        TcpListener listener = null;
+        TcpClient tcp = null;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(listener == null)
+            {
+                listener = new TcpListener(int.Parse(tbPort.Text));
+                listener.Start();
+                sbLabel1.Text = "Server Started.";
+            }
+            //      ServerProcess();
+            if (threadServer == null)
+            {
+                threadServer = new Thread(ServerProcess);
+                threadServer.Start();
+                threadRead = new Thread(ReadProcess);
+            }
+        }
+
+        void ServerProcess()
+        {
+            while (true)
+            {
+                if (listener.Pending() == true) // 접속 요청이 있는 경우...
+                {
+                    tcp = listener.AcceptTcpClient(); // 블로킹 모드
+                    AddText($"Remote EndPoint {tcp.Client.RemoteEndPoint.ToString()} 로부터 접속되었습니다.\r\n");
+                    threadRead.Start();
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        void ReadProcess()
+        {
+            if(tcp != null)
+            {
+                NetworkStream ns = tcp.GetStream();
+                byte[] bArr = new byte[50];
+                while (true)
+                {
+                    while (ns.DataAvailable)
+                    {
+                        int n = ns.Read(bArr, 0, 50);
+                        AddText(Encoding.Default.GetString(bArr, 0, n)+"\r\n");
+                        //tbReceive.Text += Encoding.Default.GetString(bArr, 0, n);
+                    }
+                }
+            }
+        }
+
+        iniFile ini = new iniFile(".\\ChatServer.ini");
+        private void frmServer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ini.SetString("Server", "Port", tbPort.Text);
+            ini.SetString("Form", "LocationX", $"{Location.X}");
+            ini.SetString("Form", "LocationY", $"{Location.Y}");
+            ini.SetString("Form", "SizeX", $"{Size.Width}");
+            ini.SetString("Form", "SizeY", $"{Size.Height}");
+
+            if (threadServer != null)
+                threadServer.Abort();
+            if (threadRead != null)
+                threadRead.Abort();
+        }
+
+        private void frmServer_Load(object sender, EventArgs e)
+        {
+            int x1, x2, y1, y2;
+            tbPort.Text = ini.GetString("Server", "Port", "9001");
+
+            x1 = int.Parse(ini.GetString("Form", "LocationX", "0"));
+            y1 = int.Parse(ini.GetString("Form", "LocationY", "0"));
+            this.Location = new Point(x1, y1);
+
+            x2 = int.Parse(ini.GetString("Form", "SizeX", "500"));
+            y2 = int.Parse(ini.GetString("Form", "SizeY", "500"));
+            this.Size = new Size(x2, y2);
+        }
+
+        private void strSend(string str)
+        {
+            if(tcp != null)
+            {
+                NetworkStream ns = tcp.GetStream(); // Read & Write
+
+                byte[] bArr = Encoding.Default.GetBytes(str);
+                ns.Write(bArr, 0, bArr.Length);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            strSend(tbSend.Text);
+        }
+
+        private void tbSend_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                int lineNo = tbSend.GetLineFromCharIndex(tbSend.SelectionStart);
+                string[] sArr = tbSend.Text.Split('\n');
+                string str = sArr[lineNo - 1] + "\n";
+                strSend(str);
+
+                //strSend(tbSend.Text.Split('\n')[tbSend.GetLineFromCharIndex(tbSend.SelectionStart) - 1] + "\n");
+            }
+        }
+
+        private void mnuSend_Click(object sender, EventArgs e)
+        {
+            strSend(tbSend.SelectedText);
+        }
+    }
+}
